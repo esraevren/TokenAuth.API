3b78a0681e8f3590495ce6911cbf329c00491ee6
esra.evren_4arctech <esra.evren_4arctech@DESKTOP-CLQAE5Q>
2024-09-22 21:18:59 +0300

    Proje dosyası ekle.

---
+++ TokenAuth.API/Models/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TokenAuth.API.Models
+{
+    public class AppDbContext(DbContextOptions options) : IdentityDbContext<AppUser, AppRole, Guid>(options)
+    {
+
+    }
+}
